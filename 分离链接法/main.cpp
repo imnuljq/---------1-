4977578366b9ff3a04ef5947227a558ac3499572
李集勤 <51432407+imnuljq@users.noreply.github.com>
2019-12-03 17:32:15 +0800

    Initial Commit

---
+++ 分离链接法/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  分离链接法
+//
+//  Created by s20181105883 on 2019/12/3.
+//  Copyright © 2019 s20181105883. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
